0cbd9dbae64c360c073f14b41c0b756f2d98eb32
ABISHEK1030 <abishekn003@gmail.com>
2025-07-18 10:04:11 +0530

    Time: 181 ms (42.75%) | Memory: 82.3 MB (50.00%) - LeetSync

---
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.java
@@ -0,0 +1,42 @@
+class Solution {
+    public long minimumDifference(int[] nums) {
+       int n = nums.length / 3;
+        long ans = Long.MAX_VALUE;
+        long leftSum = 0;
+        long rightSum = 0;
+
+        
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        
+        long[] minLeftSum = new long[nums.length];
+
+        
+        for (int i = 0; i < 2 * n; ++i) {
+            maxHeap.offer(nums[i]);
+            leftSum += nums[i];
+            if (maxHeap.size() > n) {
+                leftSum -= maxHeap.poll();
+            }
+            if (maxHeap.size() == n) {
+                minLeftSum[i] = leftSum;
+            }
+        }
+
+        
+        for (int i = nums.length - 1; i >= n; --i) {
+            minHeap.offer(nums[i]);
+            rightSum += nums[i];
+            if (minHeap.size() > n) {
+                rightSum -= minHeap.poll();
+            }
+            if (minHeap.size() == n) {
+                ans = Math.min(ans, minLeftSum[i - 1] - rightSum);
+            }
+        }
+
+        return ans;  
+    }
+}
