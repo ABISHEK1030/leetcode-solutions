954adad4d77aa40727a3be340c4d95d254d50468
ABISHEK1030 <abishekn003@gmail.com>
2025-09-15 18:22:27 +0530

    Time: 1 ms (99.30%) | Memory: 42.4 MB (53.83%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -1,21 +1,21 @@
 class Solution {
     public int canBeTypedWords(String text, String brokenLetters) {
-      boolean[] broken = new boolean[26];
+        boolean[] broken = new boolean[26];
         for (char c : brokenLetters.toCharArray()) {
-            broken[c - 'a'] = true;     
+            broken[c - 'a'] = true;         
         }
 
         int count = 0;
         for (String word : text.split(" ")) { 
             boolean valid = true;
             for (char ch : word.toCharArray()) {
-                if (broken[ch - 'a']) {      
+                if (broken[ch - 'a']) {       
                     valid = false;
                     break;
                 }
             }
-            if (valid) count++;             
+            if (valid) count++;              
         }
-        return count;   
+        return count; 
     }
 }
