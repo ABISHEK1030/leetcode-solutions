1e93ee62fcc07221051e94d2a72184205d7ea2db
ABISHEK1030 <abishekn003@gmail.com>
2025-10-01 19:41:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (95.16%) - LeetSync

--- 1642-water-bottles/water-bottles.java
+++ 1642-water-bottles/water-bottles.java
@@ -1,6 +1,6 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-       int total = numBottles;
+        int total = numBottles;
         int empty = numBottles;
         while (empty >= numExchange) {
             int newFull = empty / numExchange;
