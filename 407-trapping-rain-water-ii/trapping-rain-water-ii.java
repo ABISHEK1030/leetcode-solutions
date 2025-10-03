368d81bd2bf6f38daeb012883b7eb2a3de992ce2
ABISHEK1030 <abishekn003@gmail.com>
2025-10-03 20:59:07 +0530

    Time: 18 ms (92.95%) | Memory: 46.5 MB (49.79%) - LeetSync

---
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.java
@@ -0,0 +1,51 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    static class Cell implements Comparable<Cell> {
+        int r, c, h;
+        Cell(int r, int c, int h) { this.r = r; this.c = c; this.h = h; }
+        public int compareTo(Cell other) { return this.h - other.h; }
+    }
+
+    public int trapRainWater(int[][] heightMap) {
+        if (heightMap == null || heightMap.length == 0 || heightMap[0].length == 0) return 0;
+        int m = heightMap.length, n = heightMap[0].length;
+        if (m < 3 || n < 3) return 0;
+
+        boolean[][] visited = new boolean[m][n];
+        PriorityQueue<Cell> pq = new PriorityQueue<>();
+
+    
+        for (int i = 0; i < m; i++) {
+            pq.offer(new Cell(i, 0, heightMap[i][0]));
+            pq.offer(new Cell(i, n - 1, heightMap[i][n - 1]));
+            visited[i][0] = visited[i][n - 1] = true;
+        }
+        for (int j = 1; j < n - 1; j++) {
+            pq.offer(new Cell(0, j, heightMap[0][j]));
+            pq.offer(new Cell(m - 1, j, heightMap[m - 1][j]));
+            visited[0][j] = visited[m - 1][j] = true;
+        }
+
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        int water = 0;
+
+        while (!pq.isEmpty()) {
+            Cell cur = pq.poll();
+            for (int[] d : dirs) {
+                int nr = cur.r + d[0], nc = cur.c + d[1];
+                if (nr < 0 || nr >= m || nc < 0 || nc >= n || visited[nr][nc]) continue;
+                visited[nr][nc] = true;
+                int nh = heightMap[nr][nc];
+                if (nh < cur.h) {
+                    water += cur.h - nh;
+                    pq.offer(new Cell(nr, nc, cur.h));
+                } else {
+                    pq.offer(new Cell(nr, nc, nh));
+                }
+            }
+        }
+
+        return water;
+    }
+}
