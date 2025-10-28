17e78033daf9aabacceca76b9d5f9160d73d79ee
ABISHEK1030 <abishekn003@gmail.com>
2025-10-28 20:03:25 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (92.17%) - LeetSync

---
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countValidSelections(int[] nums) {
+         int totalSum = 0;
+        for (int x : nums) {
+            totalSum += x;
+        }
+
+        int ans = 0;
+        int leftSum = 0;
+        for (int x : nums) {
+            if (x != 0) {
+                leftSum += x;
+            } else {
+              
+                if (2 * leftSum == totalSum) {
+                   
+                    ans += 2;
+                } else if (Math.abs(2 * leftSum - totalSum) == 1) {
+                    ans += 1;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
